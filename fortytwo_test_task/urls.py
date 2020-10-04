be658f0b74bea36b68d1258e30a0317490f3fb9d
OA3IC <OA3IC.QV@gmail.com>
2020-10-04 15:22:47 +0300

    Create new app person

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.hello.views import home
+from apps.person.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
