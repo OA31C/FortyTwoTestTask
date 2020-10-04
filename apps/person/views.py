c1617bcb64d396c8682514b38341a710e2c0d341
OA3IC <OA3IC.QV@gmail.com>
2020-10-04 15:53:47 +0300

    Code the model, change the view to use the model and deploy

--- apps/person/views.py
+++ apps/person/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
-
-data = {'name': 'Max',
-        'last_name': 'Hordiychuck',
-        'date_of_birth': '1997-12-22',
-        'bio': 'Born in KK, he graduated from school in 2015. He studied in Lutsk.'
-               'Hobbies history, programming, guitar.',
-        'contacts': '+068*******',
-        'email': 'OA3IC.QV@gmail.com',
-        'jabber': 'max_hordiychuck@42cc.co',
-        'skype': 'e9ec196de1ffa713',
-        'other_contacts': 'None'
-        }
+from apps.person.models import Person
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'person/home.html', context={'data': data})
+    person = Person.objects.get(id=1)
+    return render(request, 'person/home.html', context={'person': person})
